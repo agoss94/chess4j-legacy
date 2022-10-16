2723a16e7a3adebc78520374a22c3b94fa063c5b
Andreas <andreas.goss1994@gmail.com>
2022-10-16 13:39:38 +0200

    Javadoc rook

--- src/main/java/org/chess4j/model/Rook.java
+++ src/main/java/org/chess4j/model/Rook.java
@@ -4,47 +4,80 @@ import java.util.Objects;
 
 import org.chess4j.model.Player.Color;
 
+/**
+ * A rook is a chess piece that can move only in straight lines. The class
+ * offers two static factory methods {@link #white()} and {@link #black()} for
+ * constructing a rook. Once created a rook is immutable and stateless. Also
+ * note that no two rook are equal as the class does not overwrite
+ * {@link #hashCode()} nor {@link #equals(Object)}.
+ */
 public final class Rook implements Piece {
 
-	private final Color color;
+    /**
+     * The color of the rook.
+     */
+    private final Color color;
 
-	/**
-	 * @param color
-	 */
-	private Rook(Color color) {
-		this.color = color;
-	}
+    // Private constructor
+    private Rook(Color color) {
+        this.color = color;
+    }
 
-	public static Rook white() {
-		return new Rook(Color.WHITE);
-	}
+    /**
+     * Static factory that returns a new white rook.
+     *
+     * @return a white rook.
+     */
+    public static Rook white() {
+        return new Rook(Color.WHITE);
+    }
 
-	public static Rook black() {
-		return new Rook(Color.BLACK);
-	}
+    /**
+     * Static factory that returns a new black rook.
+     *
+     * @return a black rook.
+     */
+    public static Rook black() {
+        return new Rook(Color.BLACK);
+    }
 
-	@Override
-	public boolean isValid(Tile start, Tile end) {
-		Objects.requireNonNull(start);
-		Objects.requireNonNull(end);
-		int dirX = end.column() - start.column();
-		int dirY = end.row() - start.row();
-		// A Rook can only move in one direction.
-		return dirX == 0 || dirY == 0;
-	}
+    /**
+     * {@inheritDoc}
+     *
+     * A rook move is valid in principle if the number of moved rows or the number
+     * of moved columns is zero.
+     */
+    @Override
+    public boolean isValid(Tile start, Tile end) {
+        Objects.requireNonNull(start);
+        Objects.requireNonNull(end);
+        int dirX = end.column() - start.column();
+        int dirY = end.row() - start.row();
+        // A Rook can only move in one direction.
+        return dirX == 0 || dirY == 0;
+    }
 
-	@Override
-	public Type type() {
-		return Type.ROOK;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Type type() {
+        return Type.ROOK;
+    }
 
-	@Override
-	public Color color() {
-		return color;
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Color color() {
+        return color;
+    }
 
-	@Override
-	public String toString() {
-		return color == Color.WHITE ? "\u2656" : "\u265C";
-	}
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public String toString() {
+        return color == Color.WHITE ? "\u2656" : "\u265C";
+    }
 }
